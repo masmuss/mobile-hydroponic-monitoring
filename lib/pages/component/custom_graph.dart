3cfcadcb5bec3165d8bfc50a610d9f4e1a1f3bb6
adkalfin <alfinalfarisi7@gmail.com>
2024-09-12 14:34:54 +0700

    :update

--- lib/pages/component/custom_graph.dart
+++ lib/pages/component/custom_graph.dart
@@ -1,29 +1,55 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:hydroponic/pages/common/colors.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class CustomGraph extends StatefulWidget {
+class CustomGraph extends StatelessWidget {
+  final List<Color> gradientColors = const [
+    Color.fromARGB(255, 5, 122, 189),
+    Color(0xff02d39a),
+    Color(0xffe74c3c),
+  ];
+
+  static const Map<int, String> monthMap = {
+    0: 'JAN',
+    1: 'FEB',
+    2: 'MAR',
+    3: 'APR',
+    4: 'MAY',
+    5: 'JUN',
+    6: 'JUL',
+    7: 'AUG',
+    8: 'SEP',
+    9: 'OCT',
+    10: 'NOV',
+    11: 'DEC',
+  };
+
+  static const Map<int, String> moneyMap = {
+    0: '0k',
+    1: '10k',
+    2: '20k',
+    3: '30k',
+    4: '40k',
+    5: '50k',
+    6: '60k',
+  };
+
   const CustomGraph({super.key});
 
   @override
-  State<CustomGraph> createState() => _CustomGraphState();
-}
-
-class _CustomGraphState extends State<CustomGraph> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 60.0),
-        child: AspectRatio(
-          aspectRatio: 1.70,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: AspectRatio(
+        aspectRatio: 1.5, // Menggunakan rasio aspek untuk grafik
+        child: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(18),
+            ),
+          ),
           child: Padding(
             padding: const EdgeInsets.only(
-              right: 18,
-              left: 12,
-              top: 24,
-              bottom: 12,
-            ),
+                right: 18.0, left: 12.0, top: 24, bottom: 12),
             child: LineChart(
               mainData(),
             ),
@@ -33,186 +59,111 @@ class _CustomGraphState extends State<CustomGraph> {
     );
   }
 
-  Widget bottomTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 12,
-    );
-    Widget text;
-    switch (value.toInt()) {
-      case 0:
-        text = const Text('JAN', style: style);
-        break;
-      case 1:
-        text = const Text('FEB', style: style);
-        break;
-      case 2:
-        text = const Text('MAR', style: style);
-        break;
-      case 3:
-        text = const Text('APR', style: style);
-        break;
-      case 4:
-        text = const Text('MAY', style: style);
-        break;
-      case 5:
-        text = const Text('JUN', style: style);
-        break;
-      case 6:
-        text = const Text('JUL', style: style);
-        break;
-      default:
-        text = const Text('', style: style);
-        break;
-    }
-
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      child: text,
-    );
-  }
-
-  Widget leftTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 15,
-    );
-    String text;
-    switch (value.toInt()) {
-      case 0:
-        text = '0';
-        break;
-      case 200:
-        text = '200';
-        break;
-      case 400:
-        text = '400';
-        break;
-      case 600:
-        text = '600';
-        break;
-      case 800:
-        text = '800';
-        break;
-      case 1000:
-        text = '1000';
-        break;
-      default:
-        return Container();
-    }
-
-    return Text(text, style: style, textAlign: TextAlign.left);
-  }
-
   LineChartData mainData() {
     return LineChartData(
       gridData: FlGridData(
         show: true,
         drawVerticalLine: true,
-        horizontalInterval: 200,
-        verticalInterval: 1,
         getDrawingHorizontalLine: (value) {
-          return FlLine(
-            color: BaseColors.neutral200,
+          return const FlLine(
+            color: Color.fromARGB(100, 100, 100, 100),
             strokeWidth: 1,
           );
         },
         getDrawingVerticalLine: (value) {
-          return FlLine(
-            color: BaseColors.neutral200,
+          return const FlLine(
+            color: Color.fromARGB(100, 100, 100, 100),
             strokeWidth: 1,
           );
         },
       ),
       titlesData: FlTitlesData(
         show: true,
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
         bottomTitles: AxisTitles(
           sideTitles: SideTitles(
             showTitles: true,
-            reservedSize: 30,
-            interval: 1,
-            getTitlesWidget: bottomTitleWidgets,
+            reservedSize: 32, // Mengurangi ruang untuk teks bulan
+            getTitlesWidget: (value, meta) {
+              String? month = monthMap[value.toInt()];
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  month ?? '',
+                  style: const TextStyle(
+                      color: Color(0xff68737d),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+              );
+            },
           ),
         ),
         leftTitles: AxisTitles(
           sideTitles: SideTitles(
             showTitles: true,
-            interval: 200,
-            getTitlesWidget: leftTitleWidgets,
-            reservedSize: 42,
+            reservedSize: 32, // Mengurangi ruang untuk nilai di sumbu Y
+            getTitlesWidget: (value, meta) {
+              String? money = moneyMap[value.toInt()];
+              return Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Text(
+                  money ?? '',
+                  style: const TextStyle(
+                      color: Color(0xff67727d),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+              );
+            },
           ),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
         ),
       ),
       borderData: FlBorderData(
         show: true,
-        border: Border.all(color: const Color(0xff37434d)),
+        border: Border.all(color: const Color(0xff37434d), width: 1),
       ),
       minX: 0,
-      maxX: 6,
+      maxX: 11,
       minY: 0,
-      maxY: 1000,
+      maxY: 6,
       lineBarsData: [
         LineChartBarData(
           spots: const [
-            FlSpot(0, 400),
-            FlSpot(1, 600),
+            FlSpot(0, 3),
+            FlSpot(2.6, 2),
+            FlSpot(4.9, 5),
+            FlSpot(6.8, 3.1),
+            FlSpot(8, 4),
+            FlSpot(9.5, 3),
+            FlSpot(11, 4),
           ],
           isCurved: true,
-          color: BaseColors.primary600, // Normal
+          gradient: LinearGradient(
+            colors: gradientColors,
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
           barWidth: 5,
           isStrokeCapRound: true,
-          dotData: const FlDotData(show: false),
-        ),
-        LineChartBarData(
-          spots: const [
-            FlSpot(1, 600),
-            FlSpot(2, 300),
-          ],
-          isCurved: true,
-          color: BaseColors.primary600, // Normal
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(show: false),
-        ),
-        LineChartBarData(
-          spots: const [
-            FlSpot(2, 300),
-            FlSpot(3, 200),
-            FlSpot(4, 100),
-          ],
-          isCurved: true,
-          color: Colors.yellow, // Low
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(show: false),
-        ),
-        LineChartBarData(
-          spots: const [
-            FlSpot(4, 100),
-            FlSpot(5, 500),
-          ],
-          isCurved: true,
-          color: BaseColors.primary600, // Normal
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(show: false),
-        ),
-        LineChartBarData(
-          spots: const [
-            FlSpot(5, 500),
-            FlSpot(6, 1000),
-          ],
-          isCurved: true,
-          color: Colors.red, // High
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(show: false),
+          dotData: const FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.2))
+                  .toList(),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
         ),
       ],
     );
